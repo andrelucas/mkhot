3a63b3035f1baa96fa47a0153b23f0098a3fc58e
André Lucas <andre@ae-35.com>
2019-01-16 14:18:06 +0000

    Linux fixes. Sensible clang-format settings.

--- mkhot.cpp
+++ mkhot.cpp
@@ -20,7 +20,6 @@ using namespace std::chrono_literals;
 using namespace std;
 
 #include <fcntl.h>
-#include <libgen.h>
 #include <unistd.h>
 
 #include "boost/filesystem.hpp"
@@ -29,34 +28,23 @@ using namespace std;
 namespace fs = boost::filesystem;
 namespace po = boost::program_options;
 
-enum class busymode
-{
-    none = 0,
-    wait,
-    io
-};
+enum class busymode { none = 0, wait, io };
 
-static string to_string(busymode m)
-{
-    switch (m)
-    {
-    case busymode::none:
-        return "none";
-        break;
-    case busymode::wait:
-        return "wait";
-        break;
-    case busymode::io:
-        return "io";
-        break;
+static string to_string(busymode m) {
+    switch (m) {
+        case busymode::wait:
+            return "wait";
+        case busymode::io:
+            return "io";
+        default:
+            return "none";
     }
 }
 
-struct options
-{
-    uint32_t tpc;      // Threads per core.
-    uint16_t hwthread; // Hardware threads.
-    busymode mode;     // The way we'll burn CPU.
+struct options {
+    uint32_t tpc;       // Threads per core.
+    uint16_t hwthread;  // Hardware threads.
+    busymode mode;      // The way we'll burn CPU.
 };
 
 /**
@@ -65,10 +53,8 @@ struct options
  * @param n     The thread number, ignored.
  * @param opt   Options, ignored.
  */
-void busywait(size_t n, const options opt)
-{
-    while (true)
-    {
+void busywait(size_t n, const options opt) {
+    while (true) {
     }
 }
 
@@ -78,21 +64,17 @@ void busywait(size_t n, const options opt)
  * @param n     The thread number, ignored.
  * @param opt   Options, ignored.
  */
-void busyio(size_t n, const options opt)
-{
+void busyio(size_t n, const options opt) {
     string infile{"/dev/zero"};
     int f = open(infile.c_str(), O_RDONLY);
-    if (f == -1)
-    {
+    if (f == -1) {
         throw runtime_error("Failed to open input file");
     }
 
-    while (true)
-    {
+    while (true) {
         char c[4096];
         ssize_t err = read(f, &c, sizeof(c));
-        if (err == -1)
-        {
+        if (err == -1) {
             throw runtime_error("Failed to read input file");
         }
     }
@@ -105,17 +87,15 @@ void busyio(size_t n, const options opt)
  * @param n     The thread number, ignored (but passed along).
  * @param opt   Options, ignored (but passed along).
  */
-void load(size_t n, const options opt)
-{
+void load(size_t n, const options opt) {
     // Choose. Your. Pain.
-    switch (opt.mode)
-    {
-    case busymode::io:
-        busyio(n, opt);
-        break;
-    default:
-        busywait(n, opt);
-        break;
+    switch (opt.mode) {
+        case busymode::io:
+            busyio(n, opt);
+            break;
+        default:
+            busywait(n, opt);
+            break;
     }
 }
 
@@ -124,48 +104,44 @@ void load(size_t n, const options opt)
  *
  * @param opt Options.
  */
-void generate(const options &opt)
-{
+void generate(const options &opt) {
     size_t nthread = opt.hwthread * opt.tpc;
     clog << "Starting " << nthread << " load-generator thread(s), mode '" << to_string(opt.mode) << "'" << endl;
 
     vector<thread> thr;
 
-    for (size_t n = 0; n < nthread; n++)
-    {
+    for (size_t n = 0; n < nthread; n++) {
         thr.emplace_back(load, n, opt);
     }
 
     // These threads won't exit, so this will wait forever.
-    for (auto &t : thr)
-    {
+    for (auto &t : thr) {
         t.join();
     }
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     options opt{};
     // Rely on std::thread to fill the default number of hardware threads.
     opt.hwthread = std::thread::hardware_concurrency();
     // Default is a busy-wait loop.
     bool iowait = false;
 
-    fs::path prog{fs::basename(getprogname())};
+    fs::path prog{fs::basename(argv[0])};
     string desc = "Usage: " + prog.native() + " [OPTIONS]\nWhere OPTIONS can be";
     po::options_description od{desc};
-    od.add_options()                                                                                                                          //
-        ("help", "Display this message")                                                                                                      //
-        ("threads-per-core,t", po::value<uint32_t>(&opt.tpc)->default_value(1), "Threads per hardware core/thread")                           //
-        ("override-hw-threads,h", po::value<uint16_t>(&opt.hwthread)->default_value(opt.hwthread), "Override the number of hardware threads") //
-        ("io-load,i", po::bool_switch(&iowait), "Perform I/O in the load threads")                                                            //
+    od.add_options()                                                                                                 //
+        ("help", "Display this message")                                                                             //
+        ("threads-per-core,t", po::value<uint32_t>(&opt.tpc)->default_value(1), "Threads per hardware core/thread")  //
+        ("override-hw-threads,h", po::value<uint16_t>(&opt.hwthread)->default_value(opt.hwthread),
+         "Override the number of hardware threads")                                 //
+        ("io-load,i", po::bool_switch(&iowait), "Perform I/O in the load threads")  //
         ;
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, od), vm);
     po::notify(vm);
 
-    if (vm.count("help"))
-    {
+    if (vm.count("help")) {
         clog << od << "\n";
         return 1;
     }
